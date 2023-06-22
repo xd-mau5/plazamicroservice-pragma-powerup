e7c97a6d951e038045f740cd819129b7f96f0a6c
xd-mau5 <electromusic1998@gmail.com>
2023-06-22 09:30:53 -0500

    test: added unit test for tractability

--- src/test/java/com/pragma/powerup/plazamicroservice/domain/usecase/TractabilityUseCaseTest.java
+++ src/test/java/com/pragma/powerup/plazamicroservice/domain/usecase/TractabilityUseCaseTest.java
@@ -11,6 +11,7 @@ import org.mockito.MockitoAnnotations;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
@@ -108,5 +109,47 @@ class TractabilityUseCaseTest {
         when(tractabilityPersistencePort.getAllTractabilities(0,2)).thenReturn(expectedTractabilities);
         List<Tractability> actualTractabilities = tractabilityUseCase.getAllTractabilities(0,2);
         assertEquals(expectedTractabilities, actualTractabilities);
+    }
+    @Test
+    void calculateDurationPerOrder() {
+        String expectedDuration = "1 hora";
+        when(tractabilityPersistencePort.calculateDurationPerOrder(1L)).thenReturn(expectedDuration);
+        when(tractabilityPersistencePort.calculateDurationPerOrder(2L)).thenReturn(expectedDuration);
+        String actualDuration = tractabilityUseCase.calculateDurationPerOrder(1L);
+        assertEquals(expectedDuration, actualDuration);
+        actualDuration = tractabilityUseCase.calculateDurationPerOrder(2L);
+        assertEquals(expectedDuration, actualDuration);
+    }
+    @Test
+    void calculateAverageDurationPerEmployee() {
+        List<Tractability> tractabilityList = Arrays.asList(
+                new Tractability(
+                        ObjectId.get(),
+                        1L,
+                        1L,
+                        "cliente@email.com",
+                        LocalDateTime.of(2023, 6, 2, 9, 17, 0),
+                        "En espera",
+                        "Listo",
+                        2L,
+                        "employee@email.com"
+                ),
+                new Tractability(
+                        ObjectId.get(),
+                        1L,
+                        1L,
+                        "cliente@email.com"
+                        ,LocalDateTime.of(2023, 6, 2, 10, 17, 0),
+                        "Listo",
+                        "Entregado",
+                        2L,
+                        "employee@email.com"
+                )
+        );
+        Map<Long, String> expectedDuration = Map.of(2L, "1 hora y 0 minutos");
+        when(tractabilityPersistencePort.getAllTractabilities(0,2)).thenReturn(tractabilityList);
+        when(tractabilityPersistencePort.calculateAverageDurationPerEmployee()).thenReturn(expectedDuration);
+        Map<Long, String> actualDuration = tractabilityUseCase.calculateAverageDurationPerEmployee();
+        assertEquals(expectedDuration, actualDuration);
     }
 }
